9f59e62301d1700a4c8f92e493bd71c7ab1451fa
Mathew Schwartzman <mcschwartzman@outlook.com>
2026-07-17 09:38:01 -0400

    fix: adding edges works now

--- planning_pkg/src/graph_search/Graph.cpp
+++ planning_pkg/src/graph_search/Graph.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 Node Graph::getNode(int id) {
-    Node s = this->node_lookup[id];
     return this->node_lookup[id];
 }
 
@@ -12,7 +11,7 @@ void Graph::print_lookup() {
     for (auto i : this->node_lookup) {
         cout << i.first << " ";
         for (auto j : i.second.adjacent) {
-            cout << j.id;
+            cout << j.id << " ";
         }
         cout << endl;
     }
@@ -25,33 +24,29 @@ void Graph::addEdge(int source, int destination) {
 
     if (this->node_lookup.find(source) != this->node_lookup.end()){
         // if the hashmap DOES have the node
-        s = getNode(source);
-        cout << "source adjacents: " << s.adjacent.size() << endl;
-        
+        s = getNode(source);        
     }
     else {
         s = Node(source);
-        
     }
-
-    
+    // cout << "source adjacents: " << s.adjacent.size() << endl;
+    // cout << "destination to push back: " << destination << endl;
 
     if (this->node_lookup.find(destination) != this->node_lookup.end()){
         // if the hashmap DOES have the node
+        // cout << "destination to get: " << destination << endl;
         d = getNode(destination);
     }
     else {
+        // cout << "destination to make: " << destination << endl;
         d = Node(destination);
         // cout << "node created: " << d.id << endl;
     }
 
-    
-    cout << "pushing source adjacent: " << d.id << endl;
     s.adjacent.push_back(d);
-    cout << "source adjacents: " << s.adjacent.size() << endl;
     // d.adjacent.push_back(s);
 
-    this->node_lookup.insert({source, s});
+    this->node_lookup[source] = s;
     this->node_lookup.insert({destination, d});
     
 }
@@ -65,18 +60,24 @@ bool Graph::has_dfs_path(int source, int destination) {
 }
 
 bool Graph::has_dfs_path(Node source, Node destination, unordered_set<int> visited) {
+    cout << "source: " << source.id << ", destination: " << destination.id << endl;
     if (visited.find(source.id) != visited.end()) {
+        // if source is already visited
+        cout << "source: " << source.id << " is already visited!" << endl;
         return false;
     }
     visited.insert(source.id);
     if (source.id == destination.id) {
-        cout << "source id: " << source.id << " = destination id: " << destination.id << endl;
+        cout << "source: " << source.id << " is destination!" << endl;
         return true;
     }
     for (Node child : source.adjacent){
+        cout << "checking child: " << child.id << endl;
         if (has_dfs_path(child, destination, visited)){
+            cout << "dfs path exists!" << endl;
             return true;
         }
     }
+    cout << "base case" << endl;
     return false;
 }
